2695a1099e8ede2d6ac59ed04bf6e1985568514f
eneax <19818937+eneax@users.noreply.github.com>
2020-09-26 17:30:06 +0200

    Create basic pages

---
+++ frontend-gatsby/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <p>Welcome to the homepage!</p>
+  </div>
+);
+
+export default HomePage;
---
+++ frontend-gatsby/src/pages/pizzas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PizzasPage = () => (
+  <div>
+    <p>I'm PizzasPage</p>
+  </div>
+);
+
+export default PizzasPage;
