2a2411a017c42ff94ebef359b4055e2987106d52
eneax <19818937+eneax@users.noreply.github.com>
2020-09-27 11:34:56 +0200

    Use normalize and GlobalStyles

--- frontend-gatsby/src/components/layout.js
+++ frontend-gatsby/src/components/layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import 'normalize.css';
 
+import GlobalStyles from '../styles/globalStyles';
 import Nav from './nav';
 import Footer from './footer';
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
     <Nav />
     <main>{children}</main>
     <Footer />
