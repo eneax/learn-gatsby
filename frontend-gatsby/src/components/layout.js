5c55b39dea2009e41e0a2b792b0ae603a85faf39
eneax <19818937+eneax@users.noreply.github.com>
2020-09-27 11:39:55 +0200

    Add custom Typography

--- frontend-gatsby/src/components/layout.js
+++ frontend-gatsby/src/components/layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'normalize.css';
 
 import GlobalStyles from '../styles/globalStyles';
+import Typography from '../styles/typography';
 import Nav from './nav';
 import Footer from './footer';
 
 const Layout = ({ children }) => (
   <div>
     <GlobalStyles />
+    <Typography />
     <Nav />
     <main>{children}</main>
     <Footer />
