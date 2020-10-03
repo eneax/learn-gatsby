1d7ddfcb32d3da68ee1e5a959b921b81b6764dc5
eneax <19818937+eneax@users.noreply.github.com>
2020-10-03 15:43:01 +0200

    Create topping content type with custom previews

---
+++ backend-sanity/schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as pepperIcon } from 'react-icons/fa';
+
+export default {
+  // Computer name
+  name: 'topping',
+  // Visible title
+  title: 'Toppings',
+  type: 'document',
+  icon: pepperIcon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'What is the name of the topping?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? '🥬' : ''}`,
+    }),
+  },
+};
