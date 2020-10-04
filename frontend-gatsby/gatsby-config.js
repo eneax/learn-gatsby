f78f66f4d9bf9329ffda772188b7ed339a5bdb1d
eneax <19818937+eneax@users.noreply.github.com>
2020-10-04 14:42:54 +0200

    Connect Sanity to Gatsby

--- frontend-gatsby/gatsby-config.js
+++ frontend-gatsby/gatsby-config.js
@@ -1,7 +1,25 @@
-export default {
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
+
+module.exports = {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: `http://localhost:9000`,
     description: `The best pizza place in town!`,
   },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        // https://manage.sanity.io/
+        projectId: 'xtvtsdcn',
+        dataset: 'production',
+        watchMode: true, // real time editing experience in dev mode
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
