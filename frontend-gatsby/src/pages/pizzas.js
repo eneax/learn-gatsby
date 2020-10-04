96f3aaa926fc88cce4a12b01e44dba4056059155
eneax <19818937+eneax@users.noreply.github.com>
2020-10-04 15:12:25 +0200

    Fetch data in pizzas page

--- frontend-gatsby/src/pages/pizzas.js
+++ frontend-gatsby/src/pages/pizzas.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const PizzasPage = () => (
-  <>
-    <p>I'm PizzasPage</p>
-  </>
-);
+import PizzaList from '../components/pizzaList';
+
+const PizzasPage = ({ data }) => {
+  const pizzas = data.pizzas.nodes;
+
+  return (
+    <>
+      <PizzaList pizzas={pizzas} />
+    </>
+  );
+};
 
 export default PizzasPage;
+
+export const pizzasQuery = graphql`
+  {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
