83c26456ac6e7d60f06a57fbcbaf6ec5a16f2a5c
eneax <19818937+eneax@users.noreply.github.com>
2020-10-04 16:11:08 +0200

    Style pizza grid with CSS subgrid

--- frontend-gatsby/src/components/pizzaItem.js
+++ frontend-gatsby/src/components/pizzaItem.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const PizzaStyled = styled.div`
+  display: grid;
+
+  /* if subgrid is not supported in the browser, use grid-template-rows  */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  /* Take your sizing from the PizzaGridStyled, not from PizzaStyled */
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  gap: 1rem;
+
+  h2,
+  p {
+    margin: 0;
+  }
+`;
 
 const PizzaItem = ({ pizza }) => (
-  <div>
+  <PizzaStyled>
     <Link to={`/pizza/${pizza.slug.current}`}>
       <h2>
         <span className="mark">{pizza.name}</span>
       </h2>
-      {pizza.toppings.map((topping) => topping.name).join(', ')}
-      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
     </Link>
-  </div>
+    {pizza.toppings.map((topping) => topping.name).join(', ')}
+    <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
+  </PizzaStyled>
 );
 
 export default PizzaItem;
--- frontend-gatsby/src/components/pizzaList.js
+++ frontend-gatsby/src/components/pizzaList.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import PizzaItem from './pizzaItem';
 
+const PizzaGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px; /* title, topping, image */
+`;
+
 const PizzaList = ({ pizzas }) => (
-  <div>
+  <PizzaGridStyled>
     {pizzas.map((pizza) => (
       <PizzaItem key={pizza.id} pizza={pizza} />
     ))}
-  </div>
+  </PizzaGridStyled>
 );
 
 export default PizzaList;
