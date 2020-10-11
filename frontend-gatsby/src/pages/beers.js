9f3fb8bb64ae48f488be068a583f2af5f91a04eb
eneax <19818937+eneax@users.noreply.github.com>
2020-10-11 12:06:18 +0200

    Add data and styles to BeersPage

--- frontend-gatsby/src/pages/beers.js
+++ frontend-gatsby/src/pages/beers.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
-const BeersPage = () => (
+const BeerGridStyled = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyled = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+`;
+
+const BeersPage = ({ data: { beers } }) => (
   <>
-    <p>I'm the beers page</p>
+    <h2 className="center">
+      We have {beers.totalCount} Beers available. Dine in only!
+    </h2>
+
+    <BeerGridStyled>
+      {beers.nodes.map(({ id, name, image, price, rating }) => {
+        const averageRating = Math.round(rating.average);
+
+        return (
+          <SingleBeerStyled key={id}>
+            <img src={image} alt={name} />
+            <h3>{name}</h3>
+            <span>{price}</span>
+            <p title={`${averageRating} out of 5 stars`}>
+              {`⭐`.repeat(averageRating)}
+              <span style={{ filter: `grayscale(100%)` }}>
+                {`⭐`.repeat(5 - averageRating)}
+              </span>
+              <span>({rating.reviews})</span>
+            </p>
+          </SingleBeerStyled>
+        );
+      })}
+    </BeerGridStyled>
   </>
 );
 
 export default BeersPage;
+
+export const BeersQuery = graphql`
+  query BeersQuery {
+    beers: allBeer {
+      totalCount
+      nodes {
+        id
+        name
+        image
+        price
+        rating {
+          reviews
+          average
+        }
+      }
+    }
+  }
+`;
