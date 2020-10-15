5ca9cd4cb99ff68e86cdc84b1bf99e94d95ede24
eneax <19818937+eneax@users.noreply.github.com>
2020-10-15 20:41:57 +0200

    Custom Hook for Client Side Data Fetching

--- frontend-gatsby/src/pages/index.js
+++ frontend-gatsby/src/pages/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
+import useLatestData from '../utils/useLatestData';
+
 import SEO from '../components/seo';
 
-const HomePage = () => (
-  <>
-    <SEO title="Homepage" />
-
-    <p>Welcome to the homepage!</p>
-  </>
+const CurrentlySlicing = () => (
+  <div>
+    <p>CurrentlySlicing</p>
+  </div>
 );
+
+const HotSlices = () => (
+  <div>
+    <p>HotSlices</p>
+  </div>
+);
+
+const HomePage = () => {
+  const { slicemasters, hotSlices } = useLatestData();
+
+  return (
+    <>
+      <SEO title="Homepage" />
+
+      <div className="center">
+        <h1>The Best Pizza Downtown!</h1>
+        <p>Open 11am to 11pm Every Single Day</p>
+        <div>
+          <CurrentlySlicing slicemasters={slicemasters} />
+          <HotSlices hotSlices={hotSlices} />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
