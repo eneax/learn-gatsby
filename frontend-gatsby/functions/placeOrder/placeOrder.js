1f0da92ea654e52f44d23312ea33e8265a32f022
eneax <19818937+eneax@users.noreply.github.com>
2020-10-15 15:40:49 +0200

    Create placeOrder serverless function

---
+++ frontend-gatsby/functions/placeOrder/placeOrder.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+// Create a transport for nodemailer
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+exports.handler = async (event, context) => {
+  // Send email test
+  const info = await transporter.sendMail({
+    from: "Slick's Slices <slick@example.com>",
+    to: 'orders@example.com',
+    subject: 'New Order!',
+    html: `Your new pizza order is here!`,
+  });
+
+  console.log(info);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(info),
+  };
+};
